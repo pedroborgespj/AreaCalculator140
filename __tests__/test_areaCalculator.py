da3436f149781c7bdc1c66bab44b4b9a99b557e7
Pedro Borges <pedroborgespj@gmail.com>
2024-05-28 19:39:39 -0300

    Unit tests to read a test mass from a csv file - calcular_area_retangulo + utils read csv file

--- __tests__/test_areaCalculator.py
+++ __tests__/test_areaCalculator.py
@@ -1,6 +1,7 @@
 # 1 - bibliotecas, frameworks e referencias externas
 import pytest
 from area_calculator.areaCalculator import calcular_area_quadrado, calcular_area_retangulo, calcular_area_triangulo
+from utils.utils import ler_csv
 
 # 2 - Testes
 
@@ -39,6 +40,9 @@ def testar_calcular_area_triangulo_negativo():
 
     assert resultado_esperado == resultado_obtido
 
+# Teste Baseados em Dados = Data Driven Tests (DDT) --> Massa de Teste
+# Dados em uma lista
+
 @pytest.mark.parametrize('base, resultado_esperado',
                         [
                             (3, 9),
@@ -53,3 +57,13 @@ def testar_calcular_area_quadrado_lista(base, resultado_esperado):
     resultado_obtido = calcular_area_quadrado(base)
 
     assert resultado_esperado == resultado_obtido
+
+# Dados em um arquivo csv
+
+@pytest.mark.parametrize('base, altura, resultado_esperado', ler_csv('./fixtures/massa_retangulo.csv'))
+
+def testar_calcular_area_retangulo_csv(base, altura, resultado_esperado):
+
+    resultado_obtido = calcular_area_retangulo(float(base), float(altura))
+
+    assert float(resultado_esperado) == round(resultado_obtido, 2)
